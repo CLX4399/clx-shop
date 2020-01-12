54383b65563fcf30f1ceadbb3b551adf75a04da4
CLX <whtclx@outlook.com>
2020-01-12 23:26:24 +0800

    Initial commit

---
+++ shop-registry/src/main/java/com/clx/mian/ShopRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.clx.mian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author CLX
+ * @date 2020/1/12 22:54.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ShopRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopRegistryApplication.class);
+    }
+}
