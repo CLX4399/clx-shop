fbaebbd01905a4f40b35fcbe3bb30f436da34982
CLX <whtclx@outlook.com>
2020-02-28 00:12:27 +0800

    基础架构搭建完成
    Signed-off-by: CLX <whtclx@outlook.com>
    (cherry picked from commit f8b622e18375c437d355a93c828a7a20b4cfd7d4)
    Signed-off-by: CLX <whtclx@outlook.com>

--- shop-item/shop-item-service/src/main/java/com/clx/controller/CategoryController.java
+++ shop-item/shop-item-service/src/main/java/com/clx/controller/CategoryController.java
@@ -29,7 +29,7 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("list/4561561")
+    @GetMapping("list")
     public ResponseEntity<List<Category>> queryCategoryByPid(@RequestParam("pid") Long pid) {
         if (pid == null || pid.longValue() < -1) {
             //放回400参数错误
